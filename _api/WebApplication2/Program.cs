a423b74ec87a1f68ad33e8fca06ce35cdc11695d
Samuel Thacker <thackersj@cardiff.ac.uk>
2022-11-16 14:09:32 +0000

    Adding seb's api template to the repo

---
+++ _api/WebApplication2/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+
+string connectionString = config.GetConnectionString("DefaultConnection");
+
+builder.Services.AddControllers();
+
+MariaDbServerVersion serverVersion = new MariaDbServerVersion(new Version(10, 7, 4));
+builder.Services.AddDbContext<ShopContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+        // The following three options help with debugging, but should
+        // be changed or removed for production.
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
+
+var app = builder.Build();
+
+app.MapControllers();
+app.Run();
