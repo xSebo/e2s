001061336c5ba44435c2a2a85f8f0a1a31426c84
xSebo <23667880+xSebo@users.noreply.github.com>
2022-11-22 11:59:44 +0000

    Created methods for authorities table

--- _api/WebApplication2/Repos/IAuthorities.cs
+++ _api/WebApplication2/Repos/IAuthorities.cs
@@ -1,5 +1,8 @@
-﻿namespace WebApplication2.Repos;
+﻿using WebApplication2.Models;
+
+namespace WebApplication2.Repos;
 
 public interface IAuthorities{
-    
+    public Authority? ById(int id);
+    public Authority? ByName(string name);
 }
