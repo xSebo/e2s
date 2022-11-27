62c6d3b4ffadb9658fc482d03b0d6fbe5bcb320c
c1950117 <thackerSJ@cardiff.ac.uk>
2022-11-27 14:42:05 +0000

    new accounts have their password hashed before being stored in the database. login requests now have their input password compared to the stored hash

--- database/testdb.sql
+++ database/testdb.sql
@@ -8,20 +8,20 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema mydb
 -- -----------------------------------------------------
 -- -----------------------------------------------------
--- Schema e2s
+-- Schema e2sTest
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema e2s
+-- Schema e2sTest
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `e2s`;
-CREATE SCHEMA IF NOT EXISTS `e2s` DEFAULT CHARACTER SET latin1 ;
-USE `e2s` ;
+DROP SCHEMA IF EXISTS `e2sTest`;
+CREATE SCHEMA IF NOT EXISTS `e2sTest` DEFAULT CHARACTER SET latin1 ;
+USE `e2sTest` ;
 
 -- -----------------------------------------------------
--- Table `e2s`.`authorities`
+-- Table `e2sTest`.`authorities`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e2s`.`authorities` (
+CREATE TABLE IF NOT EXISTS `e2sTest`.`authorities` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
@@ -30,9 +30,9 @@ DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `e2s`.`organisations`
+-- Table `e2sTest`.`organisations`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e2s`.`organisations` (
+CREATE TABLE IF NOT EXISTS `e2sTest`.`organisations` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `logo` VARCHAR(45) NULL DEFAULT NULL,
@@ -42,13 +42,13 @@ DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `e2s`.`users`
+-- Table `e2sTest`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e2s`.`users` (
+CREATE TABLE IF NOT EXISTS `e2sTest`.`users` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(65) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
   `authorityId` INT(11) NOT NULL,
   `organisationId` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
@@ -56,12 +56,12 @@ CREATE TABLE IF NOT EXISTS `e2s`.`users` (
   INDEX `organisationId_idx` (`organisationId` ASC) VISIBLE,
   CONSTRAINT `authorityId`
     FOREIGN KEY (`authorityId`)
-    REFERENCES `e2s`.`authorities` (`id`)
+    REFERENCES `e2sTest`.`authorities` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `organisationId`
     FOREIGN KEY (`organisationId`)
-    REFERENCES `e2s`.`organisations` (`id`)
+    REFERENCES `e2sTest`.`organisations` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -69,9 +69,9 @@ DEFAULT CHARACTER SET = latin1;
 
 
 -- -----------------------------------------------------
--- Table `e2s`.`usertokens`
+-- Table `e2sTest`.`usertokens`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e2s`.`usertokens` (
+CREATE TABLE IF NOT EXISTS `e2sTest`.`usertokens` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `userId` INT(11) NOT NULL,
   `tokenId` INT(11) NOT NULL,
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS `e2s`.`usertokens` (
   INDEX `userId_idx` (`userId` ASC) VISIBLE,
   CONSTRAINT `userId`
     FOREIGN KEY (`userId`)
-    REFERENCES `e2s`.`users` (`id`)
+    REFERENCES `e2sTest`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -94,9 +94,8 @@ insert into authorities (name) values ("Super Admin");
 insert into organisations (name,logo) values ("TestOrg1","Testimg.png");
 insert into organisations (name,logo) values ("TestOrg2","Testimg.png");
 
-insert into users (name,email,password,authorityId,organisationId) values ("Seb","seb@email.com","example",1,2);
-insert into users (name,email,password,authorityId,organisationId) values ("Sam","sam@email.com","example",2,1);
-insert into users (name,email,password,authorityId,organisationId) values ("Sam2","sam2@email.com","example",3,2);
+insert into users (name,email,password,authorityId,organisationId) values ("TestUser","user@email.com","example",1,2);
+insert into users (name,email,password,authorityId,organisationId) values ("TestAdmin","admin@email.com","example",2,2);
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
