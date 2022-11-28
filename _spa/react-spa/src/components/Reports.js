0863db5c3fa7544a15b1a743f0a57d3e04af2062
c2046006 <ManasievM@cardiff.ac.uk>
2022-11-28 11:36:36 +0200

    Finished implementation of reports page. No post request tho

--- _spa/react-spa/src/components/Reports.js
+++ _spa/react-spa/src/components/Reports.js
@@ -2,9 +2,11 @@
 import background from '../background.png';
 import Center from "./Center";
 import {
+    Button,
     Card,
-    CardContent
+    CardContent, Typography
 } from "@mui/material";
+import {DisabledByDefault} from "@mui/icons-material";
 
 const sectionStyle = {
     width: "100%",
@@ -17,13 +19,55 @@ const shadows = {
 };
 
 const Reports = () => {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const handleChange = () => {
+        setIsEnabled(!isEnabled);
+    };
+    
+    function handleClick(event){
+        console.log(event)
+    }
+    
     return (
         <div style={sectionStyle}>
             <Center>
                 <div style={shadows}>
-                <Card style={{flex:1, backgroundColor:'#d7d7d7'}}>
+                <Card style={{width:"500px", height: "500px", backgroundColor:'#d7d7d7'}}>
                     <CardContent sx={{textAlign:'center'}}>
+                        <Typography>
                         <h1>Email preferences</h1>
+                        </Typography>
+                        
+                        <label>
+                            <input type="checkbox" checked={isEnabled} onChange={handleChange}/>
+                            I would like to opt-in for emails.
+                        </label>
+                        
+                        <h3>Emails frequency:</h3>
+                        
+                        <div onClick={handleClick}>
+                        <label>
+                            <input type="checkbox" disabled={!isEnabled} id="Weekly"/>
+                            Weekly
+                        </label><br/><br/>
+
+                        <label>
+                            <input type="checkbox" disabled={!isEnabled} id="Monthly"/>
+                            Monthly
+                        </label><br/><br/>
+
+                        <label>
+                            <input type="checkbox" disabled={!isEnabled} id="Annually"/>
+                            Annually
+                        </label> <br/><br/><br/><br/><br/>
+                        
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            size="large"
+                            sx={{width: '50%'}}>Save
+                        </Button>
+                        </div>
                     </CardContent>
                 </Card>
                 </div>
