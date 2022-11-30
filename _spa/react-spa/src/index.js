f94669abc66879099b2efdbad77f37d4cc648ae7
xSebo <23667880+xSebo@users.noreply.github.com>
2022-11-30 15:20:23 +0000

    Fixed merge error

--- _spa/react-spa/src/index.js
+++ _spa/react-spa/src/index.js
@@ -1,68 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+import { AuthProvider } from './context/AuthProvider';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import reportWebVitals from './reportWebVitals';
-
-import { createRoot } from "react-dom/client";
-import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-    Link,
-    Outlet,
-    createRoutesFromElements,
-} from "react-router-dom";
-import Home from "./routes/Home";
-import Reports from "./routes/Reports";
-import Navbar from "./components/Navbar";
-import "./App.css";
-import Dashboard from "./routes/Dashboard";
-import CreateOrganisation from "./fragments/CreateOrganisation";
-import Login from "./components/Login";
-import LoadLineGraph from "./components/Graph";
-
-const AppLayout = () => (
-    <>
-        <Navbar />
-        <Outlet />
-    </>
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-const router = createBrowserRouter([
-    {
-        element: <AppLayout />,
-        children: [
-            {
-                path:"/createOrganisation",
-                element: <CreateOrganisation />,
-            },
-            {
-                path:"/graph",
-                element:<LoadLineGraph />,
-            },
-            {
-                path: "/",
-                element: <Login />,
-            },
-            {
-                path: "dashboard",
-                element: <Dashboard />,
-            },
-            {
-                path: "reports",
-                element: <Reports />,
-            },
-            
-        ],
-    },
-]);
-
-createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
-);
-
-reportWebVitals();
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
