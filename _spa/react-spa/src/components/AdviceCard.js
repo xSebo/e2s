4dccc33b2414ed785c9c0729352af77c5130b5b1
c1950117 <thackerSJ@cardiff.ac.uk>
2022-12-03 16:16:08 +0000

    Commented out dummy data and removed unnecessary code.

--- _spa/react-spa/src/components/AdviceCard.js
+++ _spa/react-spa/src/components/AdviceCard.js
@@ -1,9 +1,5 @@
-import React, {useEffect, useState} from 'react'
-
 import axios from "axios";
-import dayjs from 'dayjs';
 import {Card, CardContent, Divider, Typography} from "@mui/material"
-import shadows from "@mui/material/styles/shadows";
 import {AiOutlineArrowUp, AiOutlineArrowDown} from 'react-icons/ai';
 
 const api = axios.create({
@@ -12,38 +8,14 @@ const api = axios.create({
 })
 
 export default function LoadAdviceCard({title, text, sub}) {
-    const [data, setData] = useState([]);
 
-    // function getList() {
-    //     dataType = "windSpeed"
-    //     date1 = encodeURIComponent(time1.format())
-    //     date2 = encodeURIComponent(time2.format())
-    //     return api.get("/data/byDate?dataType=" + dataType + "&date1=" + date1 + "&date2=" + date2) //2020-12-31T17%3A00%3A00
-    //         .then(data => data.data);
-    // }
-    //
-    // function loadList(){
-    //     let mounted = true;
-    //     getList()
-    //         .then(items => {
-    //             if (mounted) {
-    //                 setData(items)
-    //             }
-    //         })
-    //     return () => mounted = false;
-    // }
-    //
-    // useEffect(() => {
-    //     loadList()
-    // }, [])
+    // const dummySub = [
+    //     { sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true },
+    //     { sTitle: "emissions", sText: "£100,000", percent: "40", arrowUp:false, good:false },
+    //     { sTitle: "lorem ipsum", sText: "£100,000", percent: "40", arrowUp:true, good:true }
+    // ];
 
-    const persons = [
-        { sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true },
-        { sTitle: "emissions", sText: "£100,000", percent: "40", arrowUp:false, good:false },
-        { sTitle: "lorem ipsum", sText: "£100,000", percent: "40", arrowUp:true, good:true }
-    ];
-
-    const Person = ({ sTitle, sText, percent, arrowUp, good }) => (
+    const Sub = ({ sTitle, sText, percent, arrowUp, good }) => (
         <div style={{display: "flex", borderBottomWidth: "1px", borderBottomColor: "grey", borderBottomStyle: "solid"}}>
             <div style={{width: "120px"}}>
                 <Typography variant="h6"  sx ={{ fontWeight:'bold'}} >
@@ -53,7 +25,6 @@ export default function LoadAdviceCard({title, text, sub}) {
                     {sText}
                 </div>
             </div>
-
             <div style={{background: good ? "hsl(140,40%,55%)" : "hsl(0,100%,65%)", marginLeft:"20px", margin:"20px", width:"50px", height:"20px"}}>
                 {arrowUp
                     ? <AiOutlineArrowUp/>
@@ -64,24 +35,12 @@ export default function LoadAdviceCard({title, text, sub}) {
         </div>
     );
 
-    const [time1, setTime1] = React.useState(dayjs('2020-01-01T00:00:00'));
-    const [time2, setTime2] = React.useState(dayjs('2020-02-01T00:00:00'));
-
-
-    const handleChange1 = (newValue) => {
-        setTime1(newValue);
-    };
-    const handleChange2 = (newValue) => {
-        setTime2(newValue);
-    }
-
     const shadows = {
         boxShadow: '1px 2px 9px #BFAFB2'
     };
+
     return (
-
         <div style={{display: "flex", flexDirection: "column", alignItems:"center"}}>
-
                 <div style={shadows}>
                     <Card sx={{ padding:1}}>
                         <CardContent sx={{}}>
@@ -96,27 +55,23 @@ export default function LoadAdviceCard({title, text, sub}) {
                                         {text}
                                     </Typography>
                                 </div>
-
+                                {/*Divider*/}
                                 <Divider orientation="vertical" flexItem />
-
                                 {/*Right-hand box*/}
                                 <div style={{paddingLeft: "10px"}}>
                                     <div>
-                                        {persons.map((p, i) => (
-                                            <Person {...p} key={i} />
+                                        {sub.map((p, i) => (
+                                            <Sub {...p} key={i} />
                                         ))}
                                     </div>
                                 </div>
                             </div>
-
                         </CardContent>
                     </Card>
                 </div>
-
-            <div style={{display:"flex", flexDirection:"row", gap:"100px", justifyContent:"space-between"}}>
-
-            </div>
-
         </div>
     )
 }
+
+// example of calling this component
+//<AdviceCard title={"Lorem ipsum"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."} sub={[{ sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true }]}></AdviceCard>
--- _spa/react-spa/src/routes/Home.js
+++ _spa/react-spa/src/routes/Home.js
@@ -1,12 +1,10 @@
 ﻿import React from 'react'
-import AdviceCard from '../components/AdviceCard'
 
 function Home(){
     return(
         <div className="home">
             <h1>Home</h1>
             <br/>
-            <AdviceCard title={"Lorem ipsum"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."}></AdviceCard>
         </div>
     ); 
 }
