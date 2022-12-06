fa37848319afe3b7464789f29877b531fc42e5ce
c1950117 <thackerSJ@cardiff.ac.uk>
2022-12-06 12:27:35 +0000

    Removed dead code that appeared after repurposing the Costs.js code when making Advice.js. *Error* In my last commit I referred to the costs page when I meant the advice page.

--- _spa/react-spa/src/fragments/user/Advice.js
+++ _spa/react-spa/src/fragments/user/Advice.js
@@ -1,24 +1,17 @@
 ﻿import React from 'react';
-import Graph from '../../components/Graph';
 import AdviceCard from '../../components/AdviceCard';
-import {FormControl, InputLabel, MenuItem, Select, Card, CardContent, Typography} from "@mui/material";
+import {Typography} from "@mui/material";
 import {ResponsiveContainer} from "recharts";
 
 const Advice = () => {
-    const [dataType, setDataType] = React.useState('chp1ElectricityGen');
 
-    const handleChange = (event) => {
-        setDataType(event.target.value);
-    };
+    // const handleChange = (event) => {
+    // };
 
-    const shadows = {
-        boxShadow: '1px 2px 9px #BFAFB2'
-    };
     return(
         <ResponsiveContainer >
             <div>
                 <div style={{display:"flex", justifyContent:"space-around"}}>
-
                     <Typography variant="h4"  sx ={{ fontWeight:'bold'}} >
                     Advice
                     </Typography>
@@ -27,24 +20,15 @@ const Advice = () => {
                 <div style={{display:"flex", justifyContent:"space-around"}}>
                     <AdviceCard title={"Lorem ipsum"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."} sub={[{ sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true }]}></AdviceCard>
                 </div>
-
                 <br/>
                 <div style={{display:"flex", justifyContent:"space-around"}}>
                     <AdviceCard title={"Lorem ipsum"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."} sub={[{ sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true }]}></AdviceCard>
                 </div>
-
                 <br/>
                 <div style={{display:"flex", justifyContent:"space-around"}}>
                     <AdviceCard title={"Lorem ipsum"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."} sub={[{ sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true }]}></AdviceCard>
                 </div>
-
-
             </div>
-
-
-
-
-
         </ResponsiveContainer>
     )
 }
