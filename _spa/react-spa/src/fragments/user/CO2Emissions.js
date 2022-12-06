31cbaf9c4f22f0e53c4fe65cc7e4e4576e5d3fe7
c2046006 <ManasievM@cardiff.ac.uk>
2022-12-06 20:59:45 +0200

    Few more minor changes

--- _spa/react-spa/src/fragments/user/CO2Emissions.js
+++ _spa/react-spa/src/fragments/user/CO2Emissions.js
@@ -1,7 +1,7 @@
 ﻿import React from 'react'
 import background from "../../static/images/background.png";
 import Center from "../../components/Center";
-import {Card, CardContent} from "@mui/material";
+import {Card, CardContent, Typography} from "@mui/material";
 import graph from "../../static/images/graph.png"
 
 function CO2Emissions(){
@@ -18,51 +18,61 @@ function CO2Emissions(){
     return(
         <div className="CO2Emissions">
             <div className="home" style={sectionStyle}>
-                <h1>CO2 Emissions</h1>
+                <div style={{display:"flex", justifyContent:"space-around"}}>
+                    <Typography variant="h4"  sx ={{ fontWeight:'bold'}} >
+                        CO2 Emissions
+                    </Typography>
+                </div>
                 <Center>
                     <div style={shadows}>
-                        <Card style={{width:"1000px", height: "300px", backgroundColor:'#ffffff'}}>
-                            <CardContent>
-                                <img src={graph} alt = "Graph" style={{float:"right"}} />
-                                <h1>Gas Consumption</h1>
-                                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/>
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
-                                    Scelerisque eu ultrices vitae auctor eu. Nibh ipsum consequat nisl vel<br/>
-                                    pretium lectus quam. Dui faucibus in ornare quam viverra orci<br/>
-                                    sagittis eu volutpat. Donec enim diam vulputate ut pharetra sit amet aliquam id.<br/>
-                                    Porta nibh venenatis cras sed felis eget velit.<br/> 
-                                    In nulla posuere sollicitudin aliquam ultrices sagittis.
-                                </span>
-                            </CardContent>
-                        </Card>,
-                        <Card style={{width:"1000px", height: "300px", backgroundColor:'#ffffff'}}>
-                            <CardContent>
-                                <img src={graph} alt = "Graph" style={{float:"right"}} />
-                                <h1>Energy Usage</h1>
-                                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/>
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
-                                    Scelerisque eu ultrices vitae auctor eu. Nibh ipsum consequat nisl vel<br/>
-                                    pretium lectus quam. Dui faucibus in ornare quam viverra orci<br/>
-                                    sagittis eu volutpat. Donec enim diam vulputate ut pharetra sit amet aliquam id.<br/>
-                                    Porta nibh venenatis cras sed felis eget velit.<br/> 
-                                    In nulla posuere sollicitudin aliquam ultrices sagittis.
-                                </span>
-                            </CardContent>
-                        </Card>,
-                        <Card style={{width:"1000px", height: "300px", backgroundColor:'#ffffff'}}>
-                            <CardContent>
-                                <img src={graph} alt = "Graph" style={{float:"right"}} />
-                                <h1>Energy Distribution</h1>
-                                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/>
-                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
-                                    Scelerisque eu ultrices vitae auctor eu. Nibh ipsum consequat nisl vel<br/>
-                                    pretium lectus quam. Dui faucibus in ornare quam viverra orci<br/>
-                                    sagittis eu volutpat. Donec enim diam vulputate ut pharetra sit amet aliquam id.<br/>
-                                    Porta nibh venenatis cras sed felis eget velit.<br/> 
-                                    In nulla posuere sollicitudin aliquam ultrices sagittis.
-                                </span>
-                            </CardContent>
-                        </Card>
+                        <div style={{display:"flex", justifyContent: "space-around"}}>
+                            <Card style={{width:"1000px", height: "300px", backgroundColor:'#ffffff'}}>
+                                <CardContent>
+                                    <img src={graph} alt = "Graph" style={{float:"right"}} />
+                                    <h1>Gas Consumption</h1>
+                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/>
+                                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
+                                        Scelerisque eu ultrices vitae auctor eu. Nibh ipsum consequat nisl vel<br/>
+                                        pretium lectus quam. Dui faucibus in ornare quam viverra orci<br/>
+                                        sagittis eu volutpat. Donec enim diam vulputate ut pharetra sit amet aliquam id.<br/>
+                                        Porta nibh venenatis cras sed felis eget velit.<br/> 
+                                        In nulla posuere sollicitudin aliquam ultrices sagittis.
+                                    </span>
+                                </CardContent>
+                            </Card>
+                        </div>,
+                        <div style={{display:"flex", justifyContent: "space-around"}}>
+                            <Card style={{width:"1000px", height: "300px", backgroundColor:'#ffffff'}}>
+                                <CardContent>
+                                    <img src={graph} alt = "Graph" style={{float:"right"}} />
+                                    <h1>Energy Usage</h1>
+                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/>
+                                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
+                                        Scelerisque eu ultrices vitae auctor eu. Nibh ipsum consequat nisl vel<br/>
+                                        pretium lectus quam. Dui faucibus in ornare quam viverra orci<br/>
+                                        sagittis eu volutpat. Donec enim diam vulputate ut pharetra sit amet aliquam id.<br/>
+                                        Porta nibh venenatis cras sed felis eget velit.<br/> 
+                                        In nulla posuere sollicitudin aliquam ultrices sagittis.
+                                    </span>
+                                </CardContent>
+                            </Card>
+                        </div>,
+                        <div style={{display:"flex", justifyContent: "space-around"}}>
+                            <Card style={{width:"1000px", height: "300px", backgroundColor:'#ffffff'}}>
+                                <CardContent>
+                                    <img src={graph} alt = "Graph" style={{float:"right"}} />
+                                    <h1>Energy Distribution</h1>
+                                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/>
+                                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br/> 
+                                        Scelerisque eu ultrices vitae auctor eu. Nibh ipsum consequat nisl vel<br/>
+                                        pretium lectus quam. Dui faucibus in ornare quam viverra orci<br/>
+                                        sagittis eu volutpat. Donec enim diam vulputate ut pharetra sit amet aliquam id.<br/>
+                                        Porta nibh venenatis cras sed felis eget velit.<br/> 
+                                        In nulla posuere sollicitudin aliquam ultrices sagittis.
+                                    </span>
+                                </CardContent>
+                            </Card>
+                        </div>
                     </div>
                 </Center>
             </div>
