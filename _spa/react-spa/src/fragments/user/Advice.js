09c7621480b815f583b0c7b7d2861a710a344789
c1950117 <thackerSJ@cardiff.ac.uk>
2022-12-06 12:19:30 +0000

    Created a costs page using our react components.

--- _spa/react-spa/src/fragments/user/Advice.js
+++ _spa/react-spa/src/fragments/user/Advice.js
@@ -1,11 +1,52 @@
-﻿import React from 'react'
+﻿import React from 'react';
+import Graph from '../../components/Graph';
+import AdviceCard from '../../components/AdviceCard';
+import {FormControl, InputLabel, MenuItem, Select, Card, CardContent, Typography} from "@mui/material";
+import {ResponsiveContainer} from "recharts";
 
-function Advice(){
+const Advice = () => {
+    const [dataType, setDataType] = React.useState('chp1ElectricityGen');
+
+    const handleChange = (event) => {
+        setDataType(event.target.value);
+    };
+
+    const shadows = {
+        boxShadow: '1px 2px 9px #BFAFB2'
+    };
     return(
-        <div className="advice">
-            <h1>Advice</h1>
-        </div>
-    );
+        <ResponsiveContainer >
+            <div>
+                <div style={{display:"flex", justifyContent:"space-around"}}>
+
+                    <Typography variant="h4"  sx ={{ fontWeight:'bold'}} >
+                    Advice
+                    </Typography>
+                </div>
+                <br/>
+                <div style={{display:"flex", justifyContent:"space-around"}}>
+                    <AdviceCard title={"Lorem ipsum"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."} sub={[{ sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true }]}></AdviceCard>
+                </div>
+
+                <br/>
+                <div style={{display:"flex", justifyContent:"space-around"}}>
+                    <AdviceCard title={"Lorem ipsum"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."} sub={[{ sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true }]}></AdviceCard>
+                </div>
+
+                <br/>
+                <div style={{display:"flex", justifyContent:"space-around"}}>
+                    <AdviceCard title={"Lorem ipsum"} text={"Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."} sub={[{ sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true }]}></AdviceCard>
+                </div>
+
+
+            </div>
+
+
+
+
+
+        </ResponsiveContainer>
+    )
 }
 
-export default Advice;
+export default Advice
