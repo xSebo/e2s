6ccf8d8fd5857cb7c6f21fb48ac5345282a3ea89
c2040622 <tolandso@cardiff.ac.uk>
2022-12-06 12:32:08 +0000

    Added initial advice card

--- _spa/react-spa/src/fragments/user/EnergyExported.js
+++ _spa/react-spa/src/fragments/user/EnergyExported.js
@@ -1,9 +1,17 @@
 ﻿import React from 'react'
+import AdviceCard from "../../components/AdviceCard"
 
 function EnergyExported(){
     return(
         <div className="EnergyExported">
             <h1>Energy Exported</h1>
+            <AdviceCard title={"Lorem ipsum"}
+                          text={"Lorem ipsum dolor sit amet, consectetur adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation."}
+                          sub={[
+                              { sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true},
+                              { sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true},
+                              { sTitle: "Gas", sText: "£100,000", percent: "40", arrowUp:true, good:true},
+                          ]} />
         </div>
     );
 }
